4bd59af65669e6bf7f8896f94330696107b5b37a
chyuhung <977599667@qq.com>
2022-02-23 11:39:10 +0800

    update structure

--- src/P96_094.java
+++ src/P96_094.java
@@ -1,22 +1,21 @@
-
 import java.util.Scanner;
 
 public class P96_094 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Input Score:");
         Scanner scan = new Scanner(System.in);
         double score = scan.nextDouble();
         System.out.println(score);
 
-        if(score == 100){
+        if (score == 100) {
             System.out.println("BWM");
-        }else if(score > 80 && score <= 99){
+        } else if (score > 80 && score <= 99) {
             System.out.println("Iphone");
-        }else if(score >= 60 && score < 80){
+        } else if (score >= 60 && score < 80) {
             System.out.println("Ipad");
-        }else if(score < 60 && score >= 0){
+        } else if (score < 60 && score >= 0) {
             System.out.println("Nothing");
-        }else {
+        } else {
             System.out.println("NO");
         }
     }
