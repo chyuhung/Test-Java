983a843d8e4149a8a7e68cb39ed400c46a87ec2d
chyuhung <977599667@qq.com>
2022-02-28 14:26:16 +0800

    优化最高分处理逻辑

--- src/com/chyuhung/test/ArrayDemo.java
+++ src/com/chyuhung/test/ArrayDemo.java
@@ -17,9 +17,14 @@ public class ArrayDemo {
         //创建成绩数组
         Double[] scoreList = new Double[studentNum];
         //读取学生成绩
+        double maxScore = 0;
         for(int i =0; i < scoreList.length; i ++){
             System.out.println("请输入第" + (i+1) + "位学生的成绩：");
             scoreList[i] = scan.nextDouble();
+            //查找最高分
+            if( scoreList[i] > maxScore){
+                maxScore = scoreList[i];
+            }
         }
         //判定成绩等级
         for (int i = 0; i < scoreList.length; i ++){
@@ -31,13 +36,6 @@ public class ArrayDemo {
                 System.out.println("第" + (i+1) + "位学生的成绩等级为C。");
             }else if (scoreList[i] >= 0 && scoreList[i] < 60){
                 System.out.println("第" + (i+1) + "位学生的成绩等级为D。");
-            }
-        }
-        //查找最高分
-        double maxScore = 0;
-        for (int i = 0; i < scoreList.length -1 ; i ++ ){
-            if( scoreList[i] > maxScore){
-                maxScore = scoreList[i];
             }
         }
         System.out.println("最高分是：" + maxScore);
