6a3179fbd1f82b6c460d3fd5db9b0d6eedfaccb1
chyuhung <977599667@qq.com>
2022-03-17 16:58:05 +0800

    规范类名

--- src/com/chyuhung/test/NineNineTable.java
+++ src/com/chyuhung/test/NineNineTable.java
@@ -1,6 +1,6 @@
 package com.chyuhung.test;
 
-public class nineNineTable {
+public class NineNineTable {
     public static void main(String[] args){
         for(int i = 1 ; i <= 9 ; i ++){
             for( int j = 1; j<= i ; j ++){
